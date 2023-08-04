c4613bcd6a5c04c44c7d58f119244d9a14f79002
Mihail Stoykov <M.Stoikov@gmail.com>
2023-08-04 18:57:17 +0300

    Update to v0.45.1 and open PR script

--- build-with-v0.45.1/update.sh
+++ build-with-v0.45.1/update.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
-set -x
 for i in `cat building.txt`; do
-	git clone $i workdir ; 
-	cd workdir;
-	git checkout -b k6.0.45.1.update ;
-	go mod edit -require go.k6.io/k6@v0.45.1 && go mod tidy;
-	git commit -a -m "Bump to k6 v0.45.1 to fix build" ;
-	gh pr create;
-	cd - ; 
+	gh repo clone ${i##https://github.com/} workdir
+	cd workdir
+	git checkout -b k6.0.45.1.update 
+	go mod edit -require go.k6.io/k6@v0.45.1 && go mod tidy
+	git commit -a -m "Bump to k6 v0.45.1 to fix build" 
+	gh pr create
+	cd - 
 	rm -rf workdir
 done 
