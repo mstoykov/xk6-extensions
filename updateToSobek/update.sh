3bc632deeb2178874dd70d2571cedff379cc5923
Mihail Stoykov <M.Stoikov@gmail.com>
2024-06-10 11:25:23 +0300

    updateToSobek: Bump k6 version

--- updateToSobek/update.sh
+++ updateToSobek/update.sh
@@ -3,7 +3,7 @@ for i in `cat list.txt`; do
 	gh repo clone ${i##https://github.com/} workdir
 	cd workdir
 	git checkout -b useSobek
-	go mod edit -require go.k6.io/k6@bd114fdbd683238fae72efddf13a56f511685d62 && go mod tidy -go=1.20
+	go mod edit -require go.k6.io/k6@1f01a9bc23653d169f16ef70e935b263e80f1a90 && go mod tidy -go=1.20
 	find . -name "*.go" -type f -exec sed -i -e 's|goja\.|sobek\.|g' {} \;
 	find . -name "*.go" -type f -exec sed -i -e 's|"github.com/dop251/goja"|"github.com/grafana/sobek"|g' {} \;
 	rm -rf ./vendor
